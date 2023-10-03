9a8b0a8ebfd1d230d9236d813827ee8c762ebd58
Denis Kamchatov <kamcatovdenis@gmail.com>
2023-10-03 20:48:02 +0300

    Created a project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
